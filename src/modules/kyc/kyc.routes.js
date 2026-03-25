ed88341dbbccbd9430079bcb014240fbea465628
dev_dimeji✔ <akannioladimeji135@gmail.com>
2026-03-25 06:52:46 +0100

    Update kyc.routes.js

--- src/modules/kyc/kyc.routes.js
+++ src/modules/kyc/kyc.routes.js
@@ -3,7 +3,7 @@ import { requireAuth } from "../../middlewares/auth.middleware.js";
 import * as kycController from "./kyc.controller.js";
 
 const router = Router();
-// Upload Kyc
+// Upload Kyc routes
 router.post("/submit", requireAuth, kycController.submitKyc);
 router.get("/status", requireAuth, kycController.getKycStatus);
 
